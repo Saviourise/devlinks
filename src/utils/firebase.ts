dc0149c5a0087c86f57395037145f8515545089c
Saviour Ise <saviourise1@gmail.com>
2024-07-25 09:29:26 +0100

    env

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "devlinks-dcc60.firebaseapp.com",
-  projectId: "devlinks-dcc60",
-  storageBucket: "devlinks-dcc60.appspot.com",
-  messagingSenderId: "343776212439",
-  appId: "1:343776212439:web:157947ef2ae7504ad1c792",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
